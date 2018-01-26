2c557e2ab7cf16a059d3e8cbb42987369a0e5583
Peter Huang <peter.huang@farferch.com>
2018-01-26 10:16:18 +0800

    add axios

--- web-vue/src/img.vue
+++ web-vue/src/img.vue
@@ -1,11 +1,10 @@
 <template>
-    <v-flex id="imgs" xs12 offset-xs-4>
-
-          <v-pagination :length="1000" v-model="page" v-on:input="changePage"></v-pagination>
-        
-    </v-flex>
+  <v-flex id="imgs" xs12 offset-xs-4>
+    <v-pagination :length="num" v-model="page" v-on:input="changePage" :total-visible="15"></v-pagination>
+  </v-flex>
 </template>
 <script>
+var axios = require('axios')
 export default {
   name: 'imgs',
   data() {
@@ -19,6 +18,19 @@ export default {
       // `this` inside methods points to the Vue instance
       console.log(event)
     }
+  },
+  beforeMount: function() {
+    this.num = 3000
+  },
+  mounted: function() {
+    axios.get('http://localhost:8091/rest/api/v1/imgs')
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
   }
 }
 
